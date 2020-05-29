6dfbff8e038fe4bbfe4f7716fe8d2a85f12eeb05
Jake Lokkesmoe <jlokkesmoe@icloud.com>
2020-05-29 14:27:34 -0500

    Previews for retailers in CMS

--- src/js/cms-preview-templates/books.js
+++ src/js/cms-preview-templates/books.js
@@ -1,10 +1,24 @@
 import React from "react";
 import format from "date-fns/format";
 
-export default class PostPreview extends React.Component {
+const Retailer = ({ name, link, imageUrl }) => (
+  <a className="w-third pa3 border-box raise" href={link}>
+    <img
+      src={imageUrl}
+      alt={name}
+      className="db mw-100 br0 center"
+      style={{ maxHeight: 100 }}
+    />
+  </a>
+);
+
+export default class BookPreview extends React.Component {
   render() {
     const { entry, widgetFor, getAsset } = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
+
+    const entryRetailers = entry.getIn(["data", "retailers"]);
+    const retailers = entryRetailers ? entryRetailers.toJS() : [];
 
     return (
       <div className="mw6 center ph3 pv4">
@@ -16,6 +30,12 @@ export default class PostPreview extends React.Component {
           <p>{entry.getIn(["data", "description"])}</p>
           {image && <img src={image} alt={entry.getIn(["data", "title"])} />}
           {widgetFor("body")}
+        </div>
+
+        <div className="mw6 flex flex-wrap items-center justify-center">
+          {retailers.map(({ name, link, imageUrl }, i) => (
+            <Retailer key={i} name={name} link={link} imageUrl={imageUrl} />
+          ))}
         </div>
       </div>
     );
