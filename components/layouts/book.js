d730f2ff5ded437a683957bdb11c480e6c246d36
Jake Lokkesmoe <jlokkesmoe@icloud.com>
2021-02-16 10:11:29 -0600

    Reformatted book summary page

--- components/layouts/book.js
+++ components/layouts/book.js
@@ -3,6 +3,7 @@ import DateComponent from "../date";
 import Markdown from "../markdown";
 import Retailer from "../retailer";
 import Endorsement from "../endorsement";
+import BookCover from "../bookCover";
 
 export default function Book({
   title,
@@ -15,39 +16,32 @@ export default function Book({
   const isReleased = releaseDate && parseISO(releaseDate) < Date.now();
 
   return (
-    <>
-      <article
-        className={"mw6 center ph3 mt4 mb5"}
-        itemScope
-        itemType="https://schema.org/Book"
-      >
-        <header>
-          <h1 className="f2 lh-title b i mb3" itemProp="name">
-            {title}
-          </h1>
-          {!isReleased && releaseDate && (
-            <p className="grey-3 b lh-title mb2">
-              Releases:{" "}
-              <DateComponent
-                dateString={releaseDate}
-                itemProp="datePublished"
-              />
-            </p>
-          )}
-        </header>
-
-        {image && (
-          <figure className="mv3">
-            <img itemProp="image" src={image} alt={`Cover for ${title}`} />
-          </figure>
+    <article
+      className="book-details"
+      itemScope
+      itemType="https://schema.org/Book"
+    >
+      <header className="header">
+        <h1 className="f2 lh-title b i mb3" itemProp="name">
+          {title}
+        </h1>
+        {!isReleased && releaseDate && (
+          <p className="grey-3 b lh-title mb2">
+            Releases:{" "}
+            <DateComponent dateString={releaseDate} itemProp="datePublished" />
+          </p>
         )}
+      </header>
 
-        <div className="cms mb4" itemProp="abstract">
-          <Markdown markdown={contentMark} />
-        </div>
-      </article>
+      <div className="image">
+        <BookCover title={`Cover for ${title}`} image={image} />
+      </div>
 
-      <section className="mt4 mb5 bg-grey-1 pv4">
+      <div className="cms body" itemProp="abstract">
+        <Markdown markdown={contentMark} />
+      </div>
+
+      <section className="mt4 mb5 bg-grey-1 pv4 purchase">
         <div className="mw6 ph3 center">
           <h2 className="f3 b lh-title primary">
             {isReleased ? "Purchase today at:" : "Pre-order now at:"}
@@ -62,12 +56,60 @@ export default function Book({
       </section>
 
       {endorsements && (
-        <section className="mw6 center ph3 mt4 mb5">
+        <section className="mt4 mb5 endorsements">
           {endorsements.map((endorsement, i) => (
             <Endorsement key={`endorsement-${i}`} {...endorsement} />
           ))}
         </section>
       )}
-    </>
+
+      <style jsx>{`
+        .book-details {
+          margin-top: var(--spacing-large);
+          margin-bottom: var(--spacing-extra-large);
+        }
+
+        @media screen and (min-width: 46em) {
+          .book-details {
+            display: grid;
+            grid-template-columns: 1fr 4rem 11rem 21rem 4rem 1fr;
+            grid-template-rows: repeat(4, auto);
+            grid-column-gap: 0px;
+            grid-row-gap: 0px;
+          }
+        }
+
+        .header,
+        .body,
+        .endorsements {
+          padding-left: var(--spacing-medium);
+          padding-right: var(--spacing-medium);
+          max-width: 32rem;
+          margin: 0 auto;
+        }
+
+        .header {
+          grid-area: 1 / 4 / 2 / 6;
+        }
+
+        .image {
+          grid-area: 1 / 2 / 3 / 4;
+          max-width: 15rem;
+          margin: 0 auto var(--spacing-medium);
+        }
+
+        .body {
+          grid-area: 2 / 4 / 3 / 6;
+        }
+
+        .purchase {
+          grid-area: 3 / 1 / 4 / 7;
+        }
+
+        .endorsements {
+          grid-area: 4 / 3 / 5 / 5;
+        }
+      `}</style>
+    </article>
   );
 }
