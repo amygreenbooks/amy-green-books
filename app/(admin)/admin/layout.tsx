4ec89595638ebe1cc0e843f95f80d890b0183e90
Jake Lokkesmoe <github@jakelokkesmoe.com>
2025-05-20 20:26:50 -0500

    Updating admin script

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -28,13 +28,8 @@ export default async function RootLayout({
       </head>
       <body>
         {children}
-
         <Script
-          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          strategy="beforeInteractive"
-        ></Script>
-        <Script
-          src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"
+          src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
           strategy="beforeInteractive"
         ></Script>
       </body>
